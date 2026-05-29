f0e042ddfa78ff2601a4355e334a6bff10d0e626
Musharaf Ali <alimusharaf725@gmail.com>
2026-05-29 23:18:32 +0530

    fix vercel cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,6 +104,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://esg-ingestion-platform-8fntmwawt-musharaf725s-projects.vercel.app/dashboard",
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
